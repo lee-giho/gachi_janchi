96c9aba835ea55c80e633e11352d872e4c57fe89
giho <ff6022@naver.com>
2025-02-18 15:47:40 +0900

    test

--- front/lib/screens/login_screen.dart
+++ front/lib/screens/login_screen.dart
@@ -584,7 +584,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               borderRadius: BorderRadius.circular(5),
                             ),
                             child: const Center(
-                              child: Text("카카오로 로그인")
+                              child: Text("테스트 화면")
                             ),
                           ),
                         ],

edd0d19e4f2ffe2604e3858cb2d688aa355cec7f
giho <ff6022@naver.com>
2025-04-04 20:01:01 +0900

    Create Review Status in Restaurant DetailReviewScreen

--- front/lib/screens/restaurant_detail_review_screen.dart
+++ front/lib/screens/restaurant_detail_review_screen.dart
@@ -19,6 +19,14 @@ class RestaurantDetailReviewScreen extends StatefulWidget {
 class _RestaurantDetailReviewScreenState extends State<RestaurantDetailReviewScreen> {
 
   List<dynamic> reviews = [];
+  List<int> ratings = [];
+  Map<int, int> ratingCounts = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0
+  };
 
   @override
   void initState() {
@@ -53,6 +61,18 @@ class _RestaurantDetailReviewScreenState extends State<RestaurantDetailReviewScr
 
         setState(() {
           reviews = data["reviews"];
+          
+          // 값 초기화
+          ratings = [];
+          ratingCounts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
+
+          for (var review in reviews) {
+            int? rating = review["review"]["rating"];
+            if (rating != null) {
+              ratings.add(rating);
+              ratingCounts[rating] = ratingCounts[rating]! + 1;
+            }
+          }
         });
       } else {
         print("리뷰 리스트 요청 실패");
@@ -62,27 +82,131 @@ class _RestaurantDetailReviewScreenState extends State<RestaurantDetailReviewScr
       ScaffoldMessenger.of(context)
         .showSnackBar(SnackBar(content: Text("네트워크 오류: ${e.toString()}")));
     }
+  }
 
+  String calculateAvgStarRating(List<int> ratings) {
+    if (ratings.isEmpty) {
+      return "0.0";
+    } else {
+      int sum = ratings.reduce((a, b) => a + b);
+      double avg = sum / ratings.length;
+      return avg.toStringAsFixed(1); // 소수점 1자리
+    }
+  }
+
+  Widget buildRatingDistribution() {
+    int total = ratings.length;
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(5, (index) {
+          int star = 5 - index;
+          int count = ratingCounts[star] ?? 0;
+          double ratio = total > 0 ? count / total : 0;
+      
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                  child: Text(
+                    "$star.0",
+                    style: const TextStyle(
+                      fontSize: 14
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: LinearProgressIndicator(
+                    value: ratio,
+                    backgroundColor: Colors.grey[300],
+                    valueColor: const AlwaysStoppedAnimation<Color>(
+                      Color.fromRGBO(122, 11, 11, 1),
+                    ),
+                    minHeight: 8,
+                  )
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  count.toString(),
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black
+                  ),
+                )
+              ],
+            ),
+          );
+        }),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: CustomScrollView(
-            slivers: [
-              SliverList.builder(
-                itemCount: reviews.length,
-                itemBuilder: (context, index) {
-                  final review = reviews[index];
-                  return ReviewTile(
-                    reviewInfo: review
-                  );
-                }
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Container( // 리뷰 현황
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Colors.grey
+                )
               )
-            ],
-          )
+            ),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column( // 평균 별점
+                    children: [
+                      Row( // 평균
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 40,
+                          ),
+                          Text( // 평균값
+                            calculateAvgStarRating(ratings),
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 5),
+                      Text( // 총 리뷰 수
+                        "${ratings.length.toString()}개의 평점",
+                        style: TextStyle(
+                          color: Colors.grey[800]
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: buildRatingDistribution()
+                ),
+              ],
+            ),
+          ),
+        ),
+        SliverList.builder(
+          itemCount: reviews.length,
+          itemBuilder: (context, index) {
+            final review = reviews[index];
+            return ReviewTile(
+              reviewInfo: review
+            );
+          }
         )
       ],
     );

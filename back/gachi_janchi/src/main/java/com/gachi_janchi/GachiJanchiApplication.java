163bc93831161bd76f6c6ae0e7fdca456cb91a93
giho <ff6022@naver.com>
2025-04-27 12:52:22 +0900

    connect MongoDB Atlas

--- back/gachi_janchi/src/main/java/com/gachi_janchi/GachiJanchiApplication.java
+++ back/gachi_janchi/src/main/java/com/gachi_janchi/GachiJanchiApplication.java
@@ -16,7 +16,7 @@ public class GachiJanchiApplication {
 
     // MYSQL_PASSWORD를 환경 변수로 설정
     System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
-    System.setProperty("MONGODB_PASSWORD", dotenv.get("MONGODB_PASSWORD"));
+    System.setProperty("MONGODB_URL", dotenv.get("MONGODB_URL"));
     System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
     System.setProperty("ACCESS_TOKEN_EXP", dotenv.get("ACCESS_TOKEN_EXP"));
     System.setProperty("REFRESH_TOKEN_EXP", dotenv.get("REFRESH_TOKEN_EXP"));

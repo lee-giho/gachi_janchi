0c4e16ff8c9e9188867915e2daa99a5c6c1d1560
giho <ff6022@naver.com>
2025-04-27 16:37:37 +0900

    Apply customException to IngredientService

--- back/gachi_janchi/src/main/java/com/gachi_janchi/service/IngredientService.java
+++ back/gachi_janchi/src/main/java/com/gachi_janchi/service/IngredientService.java
@@ -2,11 +2,11 @@ package com.gachi_janchi.service;
 
 import com.gachi_janchi.entity.Ingredient;
 import com.gachi_janchi.entity.RestaurantIngredient;
+import com.gachi_janchi.exception.CustomException;
+import com.gachi_janchi.exception.ErrorCode;
 import com.gachi_janchi.repository.RestaurantIngredientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +14,11 @@ public class IngredientService {
   private final RestaurantIngredientRepository restaurantIngredientRepository;
 
   public Ingredient findIngredientByRestaurantId(String restaurantId) {
-    Optional<RestaurantIngredient> restaurantIngredient = restaurantIngredientRepository.findByRestaurantId(restaurantId);
-    return restaurantIngredient.map(RestaurantIngredient::getIngredient).orElse(null);
+    // Optional<RestaurantIngredient> restaurantIngredient = restaurantIngredientRepository.findByRestaurantId(restaurantId);
+    // return restaurantIngredient.map(RestaurantIngredient::getIngredient).orElse(null);
+    RestaurantIngredient restaurantIngredient = restaurantIngredientRepository.findByRestaurantId(restaurantId)
+      .orElseThrow(() -> new CustomException(ErrorCode.INGREDIENT_NOT_FOUND));
+
+    return restaurantIngredient.getIngredient();
   }
 }

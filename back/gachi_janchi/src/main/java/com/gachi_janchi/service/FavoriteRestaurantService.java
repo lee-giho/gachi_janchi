172c168fb4ebeb3c8941cca0167db3b70ce309ce
giho <ff6022@naver.com>
2025-04-27 16:27:05 +0900

    Apply customException to FavoriteRestaurantService

--- back/gachi_janchi/src/main/java/com/gachi_janchi/service/FavoriteRestaurantService.java
+++ back/gachi_janchi/src/main/java/com/gachi_janchi/service/FavoriteRestaurantService.java
@@ -5,6 +5,8 @@ import com.gachi_janchi.entity.FavoriteRestaurant;
 import com.gachi_janchi.entity.Ingredient;
 import com.gachi_janchi.entity.Restaurant;
 import com.gachi_janchi.entity.User;
+import com.gachi_janchi.exception.CustomException;
+import com.gachi_janchi.exception.ErrorCode;
 import com.gachi_janchi.repository.FavoriteRestaurantRepository;
 import com.gachi_janchi.repository.RestaurantRepository;
 import com.gachi_janchi.repository.ReviewRepository;
@@ -32,16 +34,18 @@ public class FavoriteRestaurantService {
   @Transactional
   public AddFavoriteRestaurantResponse addFavoriteRestaurant(AddFavoriteRestaurantRequest addFavoriteRestaurantRequest, String token) {
     String accessToken = jwtProvider.getTokenWithoutBearer(token);
-
     String restaurantId = addFavoriteRestaurantRequest.getRestaurantId();
     String userId = jwtProvider.getUserId(accessToken);
 
     // 음식점이 존재하는지 확인
-    Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(() -> new IllegalArgumentException("해당 음식점이 존재하지 않습니다. - " + restaurantId));
+    Restaurant restaurant = restaurantRepository.findById(restaurantId)
+      // .orElseThrow(() -> new IllegalArgumentException("해당 음식점이 존재하지 않습니다. - " + restaurantId));
+      .orElseThrow(() -> new CustomException(ErrorCode.RESTAURANT_NOT_FOUND));
 
     // 이미 즐겨찾기 되어있는지 확인
     if (favoriteRestaurantRepository.findByUserIdAndRestaurantId(userId, restaurantId).isPresent()) {
-      throw new IllegalArgumentException("이미 즐겨찾기한 음식점입니다.");
+      // throw new IllegalArgumentException("이미 즐겨찾기한 음식점입니다.");
+      throw new CustomException(ErrorCode.FAVORITE_RESTAURANT_ALREADY_EXIST);
     }
 
     FavoriteRestaurant favoriteRestaurant = new FavoriteRestaurant(
@@ -88,13 +92,16 @@ public class FavoriteRestaurantService {
   public DeleteFavoriteRestaurantResponse deleteFavoriteRestaurant(DeleteFavoriteRestaurantRequest deleteFavoriteRestaurantRequest, String token) {
     String accessToken = jwtProvider.getTokenWithoutBearer(token);
     String id = jwtProvider.getUserId(accessToken);
-    User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User not found"));
+    User user = userRepository.findById(id)
+      // .orElseThrow(() -> new IllegalArgumentException("User not found"));
+      .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
     String restaurantId = deleteFavoriteRestaurantRequest.getRestaurantId();
     String userId = user.getId();
 
     if (favoriteRestaurantRepository.findByUserIdAndRestaurantId(userId, restaurantId).isEmpty()) {
-      throw new IllegalArgumentException("해당 즐겨찾기가 존재하지 않습니다.");
+      // throw new IllegalArgumentException("해당 즐겨찾기가 존재하지 않습니다.");
+      throw new CustomException(ErrorCode.FAVORITE_RESTAURANT_NOT_FOUND);
     }
     favoriteRestaurantRepository.deleteByUserIdAndRestaurantId(userId, restaurantId);
     return new DeleteFavoriteRestaurantResponse("즐겨찾기 삭제 완료");
